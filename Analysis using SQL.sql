02b10c4a82807e4a79d679555fb0fb117a030287
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-08-06 23:38:49 +0500

    Analysis in SQL

--- Analysis using SQL.sql
+++ Analysis using SQL.sql
@@ -1,3 +1,8 @@
 CREATE Database Bank_Loan_Analysis;
 
 select * from [dbo].[financial_loan_data]
+
+
+--Total Loan Applications
+
+SELECT COUNT(id) AS Total_Applications FROM [dbo].[financial_loan_data]
