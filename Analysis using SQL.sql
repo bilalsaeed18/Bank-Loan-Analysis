8617e8d3d7c29768651fbbc77a9c1b5c0903b299
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-08-06 23:39:33 +0500

    Analysis in SQL

--- Analysis using SQL.sql
+++ Analysis using SQL.sql
@@ -6,3 +6,12 @@ select * from [dbo].[financial_loan_data]
 --Total Loan Applications
 
 SELECT COUNT(id) AS Total_Applications FROM [dbo].[financial_loan_data]
+
+--MTD Loan Applications
+SELECT COUNT(id) AS Total_Applications FROM [dbo].[financial_loan_data]
+WHERE MONTH(issue_date) = 12
+
+
+--PMTD Loan Applications
+SELECT COUNT(id) AS Total_Applications FROM [dbo].[financial_loan_data]
+WHERE MONTH(issue_date) = 11
