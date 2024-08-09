574a94d9b6dd7e9e34d481a51bbc82c1f23a2437
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-08-09 22:01:33 +0500

    Update Analysis in SQL

--- Analysis using SQL.sql
+++ Analysis using SQL.sql
@@ -15,3 +15,16 @@ WHERE MONTH(issue_date) = 12
 --PMTD Loan Applications
 SELECT COUNT(id) AS Total_Applications FROM [dbo].[financial_loan_data]
 WHERE MONTH(issue_date) = 11
+
+
+--Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM [dbo].[financial_loan_data]
+
+--MTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM [dbo].[financial_loan_data]
+WHERE MONTH(issue_date) = 12
+
+--PMTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM [dbo].[financial_loan_data]
+WHERE MONTH(issue_date) = 11
+
