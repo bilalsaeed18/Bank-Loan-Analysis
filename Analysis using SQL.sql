584cef1d88106c100089ad9e815b8ef5d39fd015
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-08-13 23:16:01 +0500

    Update Analysis in SQL

--- Analysis using SQL.sql
+++ Analysis using SQL.sql
@@ -152,3 +152,39 @@ SELECT
 FROM [dbo].[financial_loan_data]
 GROUP BY address_state
 ORDER BY address_state
+
+
+
+--------------TERM------------------
+SELECT 
+	term AS Term, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM [dbo].[financial_loan_data]
+GROUP BY term
+ORDER BY term
+
+
+EMPLOYEE LENGTH
+SELECT 
+	emp_length AS Employee_Length, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY emp_length
+ORDER BY emp_length
+
+
+
+PURPOSE
+SELECT 
+	purpose AS PURPOSE, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY purpose
+ORDER BY purpose
+
